ba9baf410e6da37debdd095e5a3101d62389c181
Corey Sawin <sawincp@gmail.com>
2023-01-17 13:59:50 -0500

    add alert message to event listner

--- script.js
+++ script.js
@@ -22,11 +22,14 @@ form.addEventListener('submit', (e)=>{
     fetch(characterURL+"name="+ search + "&status="+ status +"&gender="+gender)
     .then(response => response.json())
     .then(data => data.results.forEach(characterInfo => displayCharacter(characterInfo)))
-    //.then(data => console.log(data.results))
-},)
+    
+    // alerts user if they type in a wrong character name
+    .catch((error)=>{alert('Error: Please search for a Rick and Morty Character')})
+    
+})
 
 
-// display Character Data
+// fucntion created to help display character info
 function displayCharacter(info){
     let table = document.getElementById("characterTableBody")
 
@@ -41,7 +44,7 @@ function displayCharacter(info){
     img.src = info.image
     btn1.innerText= "Character Information"
     
-   
+   // add event listner to display character info
     btn1.addEventListener('click', (e)=>{
         let species = document.createElement('ul')
         let location = document.createElement('ul')
@@ -52,7 +55,6 @@ function displayCharacter(info){
         location.innerText= "Last known location: " + info.location.name
         origin.innerText= "Origin: " + info.origin.name
         
-        //console.log(arrayEpisodes.length)
         episodes.innerText= "Number of Episodes: " + info.episode.length
         
         row.appendChild(species)
@@ -60,7 +62,7 @@ function displayCharacter(info){
         row.appendChild(origin)
         row.appendChild(episodes)
 
-
+        //allows event to only trigger once
     }, {once: true})
 
     row.appendChild(name)
